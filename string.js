92c948d53f366a57ee9e1df2b7232a01ddc38c9c
Md. Anwar Hossain <aymankajolanwar@gmail.com>
2025-09-23 01:13:27 +0600

    this is a the final update of fundamental of string

--- string.js
+++ string.js
@@ -89,5 +89,86 @@
 // console.log(firstName.concat().concat(firstName, lastName));
 
 // Include
-const firstName= 'Abir';
-console.log(firstName.includes('br'));
+// const firstName= 'Abir';
+// console.log(firstName.includes('br'));
+
+
+// reverse string
+// const sentense= 'I am learning Web Development';
+
+// let reverse= '';
+
+// for(const letter of sentense){
+//     console.log(letter);
+
+//     reverse= letter+reverse;
+// }
+
+// console.log(typeof(reverse));
+
+// string reverse
+// const sentense="I want to be a Web Developer";
+
+// let reserve= "";
+
+// for(const letter of sentense){
+//     reserve= letter+ reserve;
+// }
+// console.log(reserve);
+
+// reverse of string by for of loop
+
+// const sentense='I am want to be an Web Developer';
+
+// let reserve ="";
+
+// for(const letter of sentense){
+//     reserve=letter+reserve;
+// }
+
+// console.log(reserve);
+
+// reverse of string
+
+// const sectence = "i want to a web Developer";
+
+// let reverse= '';
+
+// for(let i=0; i<sectence.length; i++){
+//     let letter = sectence[i];
+//     reverse= letter+reverse;
+// }
+
+// console.log(reverse);
+
+
+//reverse a string using for of loop
+
+const sectence= "I want to a Web Developer";
+let reverse= '';
+
+for(const letter of sectence){
+    reverse= letter+reverse;
+}
+
+// console.log(reverse);
+
+
+//reverse a string using by for loop
+const sectence1="this is a String";
+let reserve1=``;
+
+for(let i=0; i<=sectence1.length; i++){
+    let letter = sectence1[i];
+    reserve1=letter+reserve1;
+}
+
+// console.log(reserve1);
+
+
+//reverse a string using split
+const sectence2='this is a String';
+
+const reserve2=sectence2.split('').reverse().join('');
+
+console.log(reserve2);
