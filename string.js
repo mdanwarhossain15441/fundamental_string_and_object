397dbcf7ea5e68ac4d7ad0093fa5b242875e5cbf
Md. Anwar Hossain <aymankajolanwar@gmail.com>
2025-09-22 20:48:13 +0600

    frist commit

---
+++ string.js
@@ -0,0 +1,93 @@
+// there is 3 types to write a string
+
+// const str1= 'this is a string';
+// const str2= "this is a string";
+// const str3= `this is a string`;
+
+
+
+// string length
+
+// const str1 = `dhaka`;
+// console.log(str1.length);
+
+// const str2= 'comilla';
+// console.log(str2.length);
+
+// const str3= "donia";
+// console.log(str3.length);
+
+
+
+// string Index
+
+// const capital = "dhaka";
+// console.log(capital[2]);// a
+
+// const capital1= 'chitagong';
+// console.log(capital1[4]);//length= 9 index=a
+
+
+// change the array value by index
+
+// const friends = ['ami', 'tumi', 'see', 'tara'];
+
+// friends[2]= 'mia';
+
+// console.log(friends);
+
+
+// kono string a lower case korar niyom
+// const school = 'Rajuk Uttara Model School';
+// console.log(school.toLocaleUpperCase());
+
+// const college= 'Dr. Mahbubur Rahman Molla College';
+// console.log(college.toLocaleUpperCase());
+
+// const river = 'water';
+// const ocane = " water  ";
+// console.log(ocane.trimStart());
+// if(river===ocane){
+//     console.log('nodir pani khawa jay');
+// }else{
+//     console.log('somudrer pani lobonakto');
+// }
+
+// if(river.trimEnd===ocane.trimEnd){
+//     console.log('nodir pani khawa jay');
+// }else{
+//     console.log('somudrer pani lobonakto');
+// }
+
+
+// string slice
+// const address= 'andar Killa'
+// const part=address.slice(1,4);
+// console.log(part);
+
+
+//split
+// const anwar= 'Anwar is a very honest and hard working person';
+
+// console.log(anwar.split());
+// console.log(anwar.split(''));
+// console.log(anwar.split(' '));
+// console.log(anwar.split('a'));
+// console.log(anwar.split("r"));
+// console.log(anwar.split('w'));
+// console.log(anwar.split(' '));
+
+// join
+// const realFriend = ['anwar', 'mim', 'amir', 'mukta'];
+
+// console.log(realFriend.join('|'));
+
+// const firstName='Abidur';
+// const lastName= 'Rahman';
+
+// console.log(firstName.concat(' ', lastName));
+// console.log(firstName.concat().concat(firstName, lastName));
+
+// Include
+const firstName= 'Abir';
+console.log(firstName.includes('br'));
